d8729a9dfcb1da8b4beaead3b689f817a469d389
jack.lian <jack.lian@bytedance.com>
2025-07-24 19:16:44 -0600

    BE

--- Karat/BookEndings_1.java
+++ Karat/BookEndings_1.java
@@ -1,2 +1,76 @@
-package Jack2025.Karat;public class BookEndings_1 {
+package Jack2025.Karat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class BookEndings_1 {
+    public static int findEnding(int[] endings, int[][] choices, int option) {
+        Set<Integer> visited = new HashSet<>();
+        int currentPage = 1;
+
+        while (true) {
+            if (visited.contains(currentPage)) {
+                return -1; // loop detected
+            }
+            visited.add(currentPage);
+
+            // Check if current page is an ending
+            if (contains(endings, currentPage)) {
+                return currentPage;
+            }
+
+            // Check if current page has a choice
+            int[] choice = findChoice(choices, currentPage);
+            if (choice != null) {
+                int nextPage;
+                if (option == 1) {
+                    nextPage = choice[1];
+                } else {
+                    nextPage = choice[2];
+                }
+                currentPage = nextPage;
+            } else {
+                currentPage++; // no choice, proceed to next page
+            }
+        }
+    }
+
+    private static boolean contains(int[] endings, int page) {
+        for (int end : endings) {
+            if (end == page) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int[] findChoice(int[][] choices, int page) {
+        for (int[] choice : choices) {
+            if (choice[0] == page) {
+                return choice;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // Test cases
+        int[] endings = {6, 15, 21, 30};
+
+        int[][] choices1 = {{3, 14, 2}};
+        System.out.println(findEnding(endings, choices1, 1)); // 15
+        System.out.println(findEnding(endings, choices1, 2)); // -1
+
+        int[][] choices2 = {{5, 11, 28}, {9, 19, 29}, {14, 16, 20}, {18, 7, 22}, {25, 6, 30}};
+        System.out.println(findEnding(endings, choices2, 1)); // 21
+        System.out.println(findEnding(endings, choices2, 2)); // 30
+
+        int[][] choices3 = {};
+        System.out.println(findEnding(endings, choices3, 1)); // 6
+        System.out.println(findEnding(endings, choices3, 2)); // 6
+
+        int[][] choices4 = {{2, 10, 15}, {3, 4, 10}, {4, 3, 15}, {10, 3, 15}};
+        System.out.println(findEnding(endings, choices4, 1)); // -1
+        System.out.println(findEnding(endings, choices4, 2)); // 15
+    }
 }
--- Karat/BookEndings_2.java
+++ Karat/BookEndings_2.java
@@ -1,2 +1,94 @@
-package Jack2025.Karat;public class BookEndings_2 {
+package Jack2025.Karat;
+
+import java.util.*;
+
+public class BookEndings_2 {
+    public static List<Integer> findGoodEndings(int[] goodEndings, int[] badEndings, int[][] choices) {
+        Set<Integer> goodSet = new HashSet<>();
+        for (int end : goodEndings) {
+            goodSet.add(end);
+        }
+        Set<Integer> badSet = new HashSet<>();
+        for (int end : badEndings) {
+            badSet.add(end);
+        }
+
+        Set<Integer> reachableGoodEndings = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+
+        queue.add(1);
+        visited.add(1);
+
+        while (!queue.isEmpty()) {
+            int currentPage = queue.poll();
+
+            if (goodSet.contains(currentPage)) {
+                reachableGoodEndings.add(currentPage);
+                continue;
+            }
+            if (badSet.contains(currentPage)) {
+                continue;
+            }
+
+            int[] choice = findChoice(choices, currentPage);
+            if (choice != null) {
+                int nextPage1 = choice[1];
+                int nextPage2 = choice[2];
+                if (!visited.contains(nextPage1)) {
+                    visited.add(nextPage1);
+                    queue.add(nextPage1);
+                }
+                if (!visited.contains(nextPage2)) {
+                    visited.add(nextPage2);
+                    queue.add(nextPage2);
+                }
+            } else {
+                int nextPage = currentPage + 1;
+                if (!visited.contains(nextPage)) {
+                    visited.add(nextPage);
+                    queue.add(nextPage);
+                }
+            }
+        }
+
+        List<Integer> result = new ArrayList<>(reachableGoodEndings);
+        Collections.sort(result);
+        return result;
+    }
+
+    private static int[] findChoice(int[][] choices, int page) {
+        for (int[] choice : choices) {
+            if (choice[0] == page) {
+                return choice;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] goodEndings = {10, 15, 25, 34};
+        int[] badEndings = {21, 30, 40};
+
+        int[][] choices1 = {{3, 16, 24}};
+        System.out.println(findGoodEndings(goodEndings, badEndings, choices1)); // [25]
+
+        int[][] choices2 = {{3, 16, 20}};
+        System.out.println(findGoodEndings(goodEndings, badEndings, choices2)); // []
+
+        int[][] choices3 = {{3, 2, 19}, {20, 21, 34}};
+        System.out.println(findGoodEndings(goodEndings, badEndings, choices3)); // [34]
+
+        int[][] choices4 = {};
+        System.out.println(findGoodEndings(goodEndings, badEndings, choices4)); // [10]
+
+        int[][] choices5 = {{9, 16, 26}, {14, 16, 13}, {27, 29, 28}, {28, 15, 34}, {29, 30, 38}};
+        System.out.println(findGoodEndings(goodEndings, badEndings, choices5)); // [15, 34]
+
+        int[][] choices6 = {{9, 16, 26}, {13, 31, 14}, {14, 16, 13}, {27, 12, 24}, {32, 34, 15}};
+        System.out.println(findGoodEndings(goodEndings, badEndings, choices6)); // [15, 25, 34]
+
+        int[][] choices7 = {{3, 9, 10}};
+        System.out.println(findGoodEndings(goodEndings, badEndings, choices7)); // [10]
+    }
 }
